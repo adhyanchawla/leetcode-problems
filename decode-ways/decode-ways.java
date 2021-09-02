1483d94672e8128a81a8cb89ce363960bbf7b109
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-09-02 20:28:46 +0530

    Time: 1 ms (89.06%), Space: 39.1 MB (10.17%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,30 +1,89 @@
 class Solution {
     public int numDecodings(String s) {
+        
         int[] dp = new int[s.length() + 1];
-        return countDecodings(s, dp, 0);   
+        return countDecodings_opti(s);   
     }
     
+    public int countDecodings_opti(String s) {
+        int a = 1, b = 0;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            int sum = 0;
+            char ch = s.charAt(i);
+            if(ch != '0') {
+                sum += a;
+                
+                if(i < s.length() - 1) {
+                int num1 = ((ch - '0') * 10) + s.charAt(i + 1) - '0';
+                if(num1 <= 26) {
+                    sum += b;
+                }
+            }
+            
+            }
+            b = a;
+            a = sum;
+
+        }
+        
+        return a;
+    }
+    
+    public int countDecodings_tab(String ques, int[] dp, int SI) {
+        
+        for(int i = ques.length(); i >= 0; i--) {
+            if(i == ques.length()) {
+                dp[i] = 1;
+                continue;
+            }
+            
+            int count = 0;
+            
+            char ch = ques.charAt(i);
+            if(ch != '0') {
+                count += dp[i + 1];
+            
+            if(i < ques.length() - 1) {
+                int num1 = ((ch - '0') * 10) + ques.charAt(i + 1) - '0';
+                if(num1 <= 26) {
+                    count += dp[i + 2];
+                }
+            }           
+            dp[i] = count;
+        }
+        }
+        
+        return dp[SI];
+    }
+    
+        //character ko encode kr rha hu baaki ki string encode hoke aa jayegi
     public int countDecodings(String ques, int[] dp, int idx) {
-        if(ques.length() == 0){
+        if(idx == ques.length()){
             return dp[idx] = 1;
         }
         
-        if(dp[idx] != 0) return dp[idx];
+        if(dp[idx] != -1) return dp[idx];
         
         int count = 0;
         
-        char ch = ques.charAt(0);
+        char ch = ques.charAt(idx);
+        
+        if(ch == '0') {
+            return dp[idx] = 0;
+        }
+        
         int num = (int)(ch - '0');
         
         if(num != 0) {
-            count += countDecodings(ques.substring(1), dp, idx + 1);
-        }
+            count += countDecodings(ques, dp, idx + 1);
+        }   
         
-        if(ques.length() > 1) {
-            int num1 = (ch - '0') * 10 + (ques.charAt(1) - '0');
+        
+        if(idx < ques.length() - 1) {
+            int num1 = (ch - '0') * 10 + (ques.charAt(idx + 1) - '0');
             
             if(num1 >= 10 && num1 <= 26) {
-                count += countDecodings(ques.substring(2), dp, idx + 2);
+                count += countDecodings(ques, dp, idx + 2);
             }
         }
         
