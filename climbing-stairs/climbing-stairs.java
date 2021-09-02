55fd722c40973f3253f68a5638a275fcc16eabd7
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-09-02 11:30:38 +0530

    Time: 0 ms (100.00%), Space: 35.5 MB (84.32%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int climbStairs(int N) {
-        int a = 1, b = 1;
-        for(int n = 2; n <= N; n++) {
-            int sum = a + b;
-            a = b;
-            b = sum;
+    public int climbStairs(int n) {
+        return fibo_mem(n, new int[n + 1]);   
+    }
+    
+    public static int fibo_mem(int n, int[] dp) {
+        if(n <= 1) {
+            return dp[n] = 1;
         }
 
-        return b;
+        if(dp[n] != 0) return dp[n];
+
+        int ans = fibo_mem(n - 1, dp) + fibo_mem(n - 2, dp);
+
+        return dp[n] = ans;
     }
 }
