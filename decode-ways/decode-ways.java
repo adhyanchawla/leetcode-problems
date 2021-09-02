85622617bd434027f8076efa246ffe2edcd32f25
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-09-02 19:17:18 +0530

    Time: 122 ms (5.17%), Space: 113.8 MB (5.45%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,45 +1,33 @@
 class Solution {
     public int numDecodings(String s) {
-        
-        int[] qb = new int[s.length()];
-        Arrays.fill(qb, -1);
-        int count = countEncodings(s, "", qb, 0);
-        
-        return count;
+        int[] dp = new int[s.length() + 1];
+        return countDecodings(s, dp, 0);   
     }
     
-    public int countEncodings(String ques, String ans, int[] qb, int p)
-    {
-        if(ques.length() == 0)
-        {
-            return 1;
+    public int countDecodings(String ques, int[] dp, int idx) {
+        if(ques.length() == 0){
+            return dp[idx] = 1;
         }
+        
+        if(dp[idx] != 0) return dp[idx];
         
         int count = 0;
-        if(qb[p] != -1)
-        {
-            return qb[p];
-        }
+        
         char ch = ques.charAt(0);
         int num = (int)(ch - '0');
-        if(num != 0)
-        {
-            char c1 = (char)(num + 'a' - 1);
-            count += countEncodings(ques.substring(1), ans + c1, qb, p+1);  
+        
+        if(num != 0) {
+            count += countDecodings(ques.substring(1), dp, idx + 1);
         }
         
-        
-        if(ques.length() > 1)
-        {
-            int num1 = num * 10 + (ques.charAt(1) - '0');
-            if(num1 >= 10 && num1 <= 26)
-            {
-                char c2 = (char)(num1 + 'a' -1);
-                count += countEncodings(ques.substring(2), ans + c2, qb, p+2);
+        if(ques.length() > 1) {
+            int num1 = (ch - '0') * 10 + (ques.charAt(1) - '0');
+            
+            if(num1 >= 10 && num1 <= 26) {
+                count += countDecodings(ques.substring(2), dp, idx + 2);
             }
         }
         
-        qb[p] = count;
-        return count;
+        return dp[idx] = count;
     }
 }
