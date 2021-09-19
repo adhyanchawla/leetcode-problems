8ac2688a2cf29bbfca26cf449dc2ca12f86caa05
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-09-19 10:01:10 +0530

    Time: 6 ms (46.34%), Space: 40.5 MB (29.90%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,18 +1,37 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        int[] ans = new int[2];
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
         
         for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    ans[0] = i;
-                    ans[1] = j;
-                    break;
-                }
+            if(!map.containsKey(nums[i])) {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(nums[i], list);
+            } else {
+                ArrayList<Integer> list = map.get(nums[i]);
+                list.add(i);
+                map.put(nums[i], list);
             }
         }
         
+        int[] ans = new int[2];
+        for(int ele : map.keySet()) {
+            if(map.containsKey(target - ele)) {
+                ArrayList<Integer> list = map.get(ele);
+                if(target - ele == ele && list.size() == 2) {
+                    ans[0] = list.get(0);
+                    ans[1] = list.get(1);
+                    return ans;
+                } else {
+                    ArrayList<Integer> list1 = map.get(target - ele);
+                    int idx1 = list1.get(0);
+                    int idx = list.get(0);
+                    ans[0] = idx;
+                    ans[1] = idx1;
+                    return ans;
+                }
+            }
+        }
         return ans;
     }
 }
