c3693b58eda6b8565e919ed894978072724c18c6
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-09-27 09:26:23 +0530

    Time: 1 ms (37.34%), Space: 38.4 MB (48.42%) - LeetHub

--- unique-paths-ii/unique-paths-ii.java
+++ unique-paths-ii/unique-paths-ii.java
@@ -1,36 +1,43 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dir = {{0, 1}, {1, 0}};
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {        
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
         
-        return mazePathsWithOneJump_tab(obstacleGrid, 0, 0, m - 1, n - 1, new int[m][n], dir);
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1){
+            return 0;
+        }
+        
+        int[][] dir = {{0, 1}, {1, 0}};
+        int[][]dp = new int[m][n];
+        //System.out.println(mazePathOneJump_memo(dir, dp, 0, 0, 2, 2));
+        return mazePathWithOneJump_tab(obstacleGrid, dir, dp, 0, 0, m - 1, n - 1);
+        //display2D(dp);   
     }
     
-    public static int mazePathsWithOneJump_tab(int[][] obstacleGrid, int SR, int SC, int ER, int EC, int[][] dp, int[][] dir) {
-        for(int sr = ER; sr >= SR; sr--) {
-            for(int sc = EC; sc >= SC; sc--) {
-                if(sr == ER && sc == EC) {
-                    dp[sr][sc] = 1;
+    public static int mazePathWithOneJump_tab(int[][] board, int[][] dir, int [][] dp, int sr, int sc, int er, int ec) {
+
+        for(int i = er; i >= sr; i--) {
+            for(int j = ec; j >= sc; j--) {
+                if(i == er && j == ec) {
+                    dp[i][j] = 1;
                     continue;
                 }
 
                 int count = 0;
+
                 for(int d = 0; d < dir.length; d++) {
-                    int r = sr + dir[d][0];
-                    int c = sc + dir[d][1];
-                    if(r >= 0 && c >= 0 && r < dp.length && c < dp[0].length && obstacleGrid[r][c] == 0) {
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+
+                    if(r >= 0 && c >= 0 && r <= er && c <= ec && board[r][c] != 1) {
                         count += dp[r][c];
                     }
-    
                 }
-    
-                dp[sr][sc] = count;
+        
+                dp[i][j] = count;
             }
-
+            
         }
 
-        return dp[SR][SC];
+        return dp[0][0];
     }
 }
