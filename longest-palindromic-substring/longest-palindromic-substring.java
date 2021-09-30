3df8a95ffd8429b5434a62f38e64692cec59aee9
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-10-01 00:00:13 +0530

    Time: 298 ms (22.40%), Space: 49.5 MB (20.20%) - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        return lps(s, 0, n - 1, dp);
+    }
+    
+    public String lps(String s, int I, int J, int[][] dp) {
+        int n = s.length();
+        for(int gap = 0; gap < n; gap++) {
+            for(int i = 0, j = gap; i < n && j < n; i++, j++) {
+                if(i == j) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                if(gap == 1) {
+                    if(s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = 2;
+                        continue;
+                    }
+                }
+                
+                if(s.charAt(i) == s.charAt(j)) {
+                    if(dp[i + 1][j - 1] != 0)
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    else continue;
+                }
+            }
+        }
+        
+        
+        int max = 0;
+        
+        int mi = 0, mj = 0;
+        
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp.length; j++) {
+                if(dp[i][j] > max) {
+                    max = dp[i][j];
+                    mi = i;
+                    mj = j;
+                }
+            }
+            
+        }
+        
+
+        
+        return s.substring(mi, mj + 1);
+    }
+}
