d2a7599779f7ed4a2777bfe2d1ac90b58aa6bf6b
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-10-09 14:06:19 +0530

    Time: 0 ms (100.00%), Space: 37.3 MB (73.95%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,89 +1,31 @@
 class Solution {
     public int numDecodings(String s) {
-        
-        int[] dp = new int[s.length() + 1];
-        return countDecodings_opti(s);   
+        int[]dp = new int[s.length() + 1];
+        Arrays.fill(dp , -1);
+        return decodeWays(s, 0, dp);
     }
     
-    public int countDecodings_opti(String s) {
-        int a = 1, b = 0;
-        for(int i = s.length() - 1; i >= 0; i--) {
-            int sum = 0;
-            char ch = s.charAt(i);
-            if(ch != '0') {
-                sum += a;
-                
-                if(i < s.length() - 1) {
-                int num1 = ((ch - '0') * 10) + s.charAt(i + 1) - '0';
-                if(num1 <= 26) {
-                    sum += b;
-                }
-            }
-            
-            }
-            b = a;
-            a = sum;
-
-        }
-        
-        return a;
-    }
     
-    public int countDecodings_tab(String ques, int[] dp, int SI) {
-        
-        for(int i = ques.length(); i >= 0; i--) {
-            if(i == ques.length()) {
-                dp[i] = 1;
-                continue;
-            }
-            
-            int count = 0;
-            
-            char ch = ques.charAt(i);
-            if(ch != '0') {
-                count += dp[i + 1];
-            
-            if(i < ques.length() - 1) {
-                int num1 = ((ch - '0') * 10) + ques.charAt(i + 1) - '0';
-                if(num1 <= 26) {
-                    count += dp[i + 2];
-                }
-            }           
-            dp[i] = count;
-        }
-        }
-        
-        return dp[SI];
-    }
-    
-        //character ko encode kr rha hu baaki ki string encode hoke aa jayegi
-    public int countDecodings(String ques, int[] dp, int idx) {
-        if(idx == ques.length()){
+    public int decodeWays(String s, int idx, int[] dp) {
+        if(idx >= s.length()) {
             return dp[idx] = 1;
         }
         
-        if(dp[idx] != -1) return dp[idx];
+        if(dp[idx] != -1) {
+            return dp[idx];
+        }
         
         int count = 0;
         
-        char ch = ques.charAt(idx);
-        
-        if(ch == '0') {
-            return dp[idx] = 0;
+        char ch = s.charAt(idx);
+        if(ch - '0' != 0) {
+            count += decodeWays(s, idx + 1, dp);
         }
         
-        int num = (int)(ch - '0');
-        
-        if(num != 0) {
-            count += countDecodings(ques, dp, idx + 1);
-        }   
-        
-        
-        if(idx < ques.length() - 1) {
-            int num1 = (ch - '0') * 10 + (ques.charAt(idx + 1) - '0');
-            
-            if(num1 >= 10 && num1 <= 26) {
-                count += countDecodings(ques, dp, idx + 2);
+        if(idx < s.length() - 1) {
+            int num = (ch - '0') * 10 + (s.charAt(idx + 1) - '0');
+            if(num >= 10 && num <= 26) {
+                count += decodeWays(s, idx + 2, dp);
             }
         }
         
