2ec4c7af2850243897010743593f04fa425cefd4
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-10-11 06:52:43 +0530

    Time: 66 ms (43.98%), Space: 77.2 MB (16.77%) - LeetHub

--- longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,27 +1,29 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        int[][] dp = new int[n][n];
+        int[][] dp = new int[s.length()][s.length()];
         
-        return lpss_tab(s, 0, n - 1, dp);
+        for(int[]d : dp) {
+            Arrays.fill(d, -1);
+        }
+        
+        return lpss(s, dp, 0, s.length() - 1);
     }
     
-    public int lpss_tab(String str, int I, int J, int[][] dp) {
-        int n = str.length();
-        for(int gap = 0; gap < n; gap++) {
-            for(int i = 0, j = gap; i < n && j < n; i++, j++) {
-                if(i >= j) {
-                    dp[i][j] = (i == j) ? 1 : 0; 
-                    continue;
-                }
-        
-                if(str.charAt(i) == str.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
-                } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                }
-            }
+    public int lpss(String s, int[][] dp, int i, int j) {
+        if(i >= j) {
+            return dp[i][j] = (i == j) ? 1 : 0;
         }
-        return dp[I][J];
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        if(s.charAt(i) == s.charAt(j)) {
+            return dp[i][j] = 2 + lpss(s, dp, i + 1, j - 1);
+        } else {
+            return dp[i][j] = Math.max(lpss(s, dp, i + 1, j), lpss(s, dp, i, j - 1));
+        }
+        
+        
     }
 }
