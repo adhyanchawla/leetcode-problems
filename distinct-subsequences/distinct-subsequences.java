d31bed39a33f57b181fc2a11c766a596d4a7f6ee
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-10-11 23:21:46 +0530

    Time: 38 ms (8.55%), Space: 47.2 MB (5.12%) - LeetHub

--- distinct-subsequences/distinct-subsequences.java
+++ distinct-subsequences/distinct-subsequences.java
@@ -3,16 +3,42 @@ class Solution {
         int n = s.length(), m = t.length();
         int[][] dp = new int[n + 1][m + 1];
         
-        for(int[] d : dp) {
-            Arrays.fill(d, -1);
-        }
+        // for(int[] d : dp) {
+        //     Arrays.fill(d, -1);
+        // }
         
-        return distinct(s, t, n, m, dp);
+        return dis_tab(s, t, n, m, dp);
+    }
+    
+    public static int dis_tab(String s1, String s2, int N, int M, int[][] dp) {
+        for(int n = 0; n <= N; n++) {
+            for(int m = 0; m <= M; m++) {
+                if(m == 0 || n == 0 || m > n) {
+                    dp[n][m] = (m == 0) ? 1 : 0;
+                    continue;
+                }
+
+                int a = dp[n - 1][m - 1];
+                int b = dp[n - 1][m];
+
+                if(s1.charAt(n - 1) == s2.charAt(m - 1)) {
+                    dp[n][m] = a + b;
+                } else {
+                    dp[n][m] = b;
+                }
+            }
+        }
+
+        return dp[N][M];
     }
     
     public int distinct(String s1, String s2, int n, int m, int[][] dp) {
         if(n == 0 || m == 0) {
             return dp[n][m] = (m == 0) ? 1 : 0;
+        }
+        
+        if(m > n) {
+            return dp[n][m] = 0;
         }
         
         
