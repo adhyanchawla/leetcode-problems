c7fdbc18715d07f50814d21f4bb3119386be725d
Adhyan Chawla <51983961+adhyanchawla@users.noreply.github.com>
2021-10-11 12:05:24 +0530

    Time: 446 ms (16.15%), Space: 124.2 MB (5.01%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,52 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        int[][] dp = new int[n][n];
-        return lps(s, 0, n - 1, dp);
-    }
-    
-    public String lps(String s, int I, int J, int[][] dp) {
-        int n = s.length();
+        int[][] dp = new int[s.length()][s.length()];
+        
+        String ans = "";
+        int si = 0;
+        int ei = 0;
         for(int gap = 0; gap < n; gap++) {
             for(int i = 0, j = gap; i < n && j < n; i++, j++) {
-                if(i == j) {
+                if(gap == 0) {
                     dp[i][j] = 1;
                     continue;
-                }
+                } 
                 
                 if(gap == 1) {
                     if(s.charAt(i) == s.charAt(j)) {
                         dp[i][j] = 2;
-                        continue;
                     }
+                } 
+                
+                if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] != 0) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
                 }
                 
-                if(s.charAt(i) == s.charAt(j)) {
-                    if(dp[i + 1][j - 1] != 0)
-                        dp[i][j] = dp[i + 1][j - 1] + 2;
-                    else continue;
+                if(dp[i][j] > 0 && dp[i][j] > ans.length()) {
+                    si = i;
+                    ei = j;
                 }
             }
         }
         
-        
-        int max = 0;
-        
-        int mi = 0, mj = 0;
-        
-        for(int i = 0; i < dp.length; i++) {
-            for(int j = 0; j < dp.length; j++) {
-                if(dp[i][j] > max) {
-                    max = dp[i][j];
-                    mi = i;
-                    mj = j;
-                }
-            }
-            
-        }
-        
-
-        
-        return s.substring(mi, mj + 1);
+        return s.substring(si, ei + 1);
     }
 }
